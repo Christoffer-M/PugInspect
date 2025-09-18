6bccb37bfe09302f5f0cd6d4ad4749287ea86be3
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-18 20:01:16 +0200

    formatting

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,30 +1,33 @@
-import { createFileRoute } from '@tanstack/react-router'
-import '../App.css'
-import { Autocomplete, Box, Center, Flex, Title } from '@mantine/core'
+import { createFileRoute } from "@tanstack/react-router";
+import "../App.css";
+import { Autocomplete, Box, Center, Flex, Title } from "@mantine/core";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: App,
-})
+});
 
 function App() {
   return (
-    <Center h={'100vh'} >
+    <Center h={"100vh"}>
       <Flex direction="column" align="center" gap="md" mb="xl">
         <Flex direction="column" align="center" gap="sm">
           <Title order={1}>Welcome to PugInspect</Title>
-          <Title order={3}>The ultimate Pug template linter and inspector</Title>
+          <Title order={3}>
+            The ultimate Pug template linter and inspector
+          </Title>
         </Flex>
         <Flex direction="column" align="center" gap="sm" mt="xl">
           <Autocomplete
             placeholder="Search for a Pug template"
-            data={['index.pug', 'layout.pug', 'header.pug', 'footer.pug']}
+            data={["index.pug", "layout.pug", "header.pug", "footer.pug"]}
             style={{ width: 300 }}
           />
-      <Box mt="md" style={{ color: 'gray' }}>
-        Start by typing in a character name above to search for a Pug template.
-      </Box>
-    </Flex>
-      </Flex>     
+          <Box mt="md" style={{ color: "gray" }}>
+            Start by typing in a character name above to search for a Pug
+            template.
+          </Box>
+        </Flex>
+      </Flex>
     </Center>
-  )
+  );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tanstackRouter from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tanstackRouter from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact()],
-})
+});
