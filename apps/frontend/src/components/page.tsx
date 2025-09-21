b3240b6e06287cd363a2191c4a356ae683c9ed2e
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-21 20:14:49 +0200

    feat: create Page component and update import path in routes

--- apps/frontend/src/components/page.tsx
+++ apps/frontend/src/components/page.tsx
@@ -1,5 +1,5 @@
 import { AppShell, Typography } from "@mantine/core";
-import Header from "./components/header";
+import Header from "./header";
 
 export const Page: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <AppShell header={{ height: 60 }}>
