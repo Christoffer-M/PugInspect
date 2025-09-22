95fd569f0d977ff600e2be25a8080f4ddebf3114
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-22 18:46:56 +0200

    clean up bundles

---
+++ .graphqlrc.ts
@@ -0,0 +1,13 @@
+export default {
+  projects: {
+    web: {
+      schema: ["./packages/graphql-types/src/schema.graphql"],
+      documents: ["./apps/frontend/src/**/*.{ts,tsx,js,jsx}"],
+    },
+    api: {
+      schema:
+        "apps/backend/src/schema/services/warcraftLogs/generated/schema.graphql",
+      documents: ["apps/backend/src/**/*.{ts,tsx,js,jsx}"],
+    },
+  },
+};
