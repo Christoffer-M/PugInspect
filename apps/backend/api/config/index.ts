979cebcb17db3c1f8d3be301e488392faa8be666
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-23 22:51:22 +0200

    refactor: Simplify environment variable assignment in config

--- apps/backend/api/config/index.ts
+++ apps/backend/api/config/index.ts
@@ -2,7 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const raiderIoApiKey = process.env.RAIDERIO_API_KEY;
+const warcraftLogsBearerToken = process.env.WARCRAFTLOGS_BEARER_TOKEN;
+
 export const config = {
-  raiderIoApiKey: process.env.RAIDERIO_API_KEY,
-  warcraftLogsBearerToken: process.env.WARCRAFTLOGS_BEARER_TOKEN,
+  raiderIoApiKey,
+  warcraftLogsBearerToken,
 };
