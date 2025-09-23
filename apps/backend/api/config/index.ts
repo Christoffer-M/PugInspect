ddfb4a5a42ae74b994eaeeaf3427b6908bd2dcb0
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-23 22:42:12 +0200

    fix: Remove default empty string fallback for API

--- apps/backend/api/config/index.ts
+++ apps/backend/api/config/index.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  raiderIoApiKey: process.env.RAIDERIO_API_KEY || "",
-  warcraftLogsBearerToken: process.env.WARCRAFTLOGS_BEARER_TOKEN || "",
+  raiderIoApiKey: process.env.RAIDERIO_API_KEY,
+  warcraftLogsBearerToken: process.env.WARCRAFTLOGS_BEARER_TOKEN,
 };
