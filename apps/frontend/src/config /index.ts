1dc2a3bca3c912497359e20f59ca23c78b2125cc
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-24 18:37:54 +0200

    refactor: Remove dotenv configuration and use VITE_API_URL for apiUrl

--- apps/frontend/src/config /index.ts
+++ apps/frontend/src/config /index.ts
@@ -1,9 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const apiUrl = process.env.API_URL ?? "http://localhost:4000/graphql";
-
 export const config = {
-  apiUrl: apiUrl,
+  apiUrl: import.meta.env.VITE_API_URL,
 };
