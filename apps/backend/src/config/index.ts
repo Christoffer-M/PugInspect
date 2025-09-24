02ef29b3d7b1bd0738566ad30a1e9b47fdd21c8f
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-24 17:40:59 +0200

    Remove pnpm workspace configuration file to eliminate ignored built dependencies.

--- apps/backend/src/config/index.ts
+++ apps/backend/src/config/index.ts
@@ -4,8 +4,10 @@ dotenv.config();
 
 const raiderIoApiKey = process.env.RAIDERIO_API_KEY;
 const warcraftLogsBearerToken = process.env.WARCRAFTLOGS_BEARER_TOKEN;
+const port = Number.parseInt(process.env.PORT ?? "4000");
 
 export const config = {
   raiderIoApiKey,
   warcraftLogsBearerToken,
+  port,
 };
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import typeDefs from "./schema/typeDefs.js";
 import resolvers from "./schema/resolvers.js";
+import { config } from "./config/index.js";
 
 const server = new ApolloServer({
   typeDefs,
@@ -9,7 +10,7 @@ const server = new ApolloServer({
 });
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: config.port },
   context: async () => ({
     // You can pass auth, loaders, cache here
   }),
