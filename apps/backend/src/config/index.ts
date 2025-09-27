d82098d815d785c4d1c97d96ad2417db605ae5d7
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-27 17:48:36 +0200

    add bearer token config only for codegen

--- apps/backend/src/config/index.ts
+++ apps/backend/src/config/index.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const raiderIoApiKey = process.env.RAIDERIO_API_KEY;
 const warcraftLogsClientId = process.env.WARCRAFTLOGS_CLIENT_ID;
 const warcraftLogsClientSecret = process.env.WARCRAFTLOGS_CLIENT_SECRET;
+const warcraftLogsBearerToken = process.env.WARCRAFTLOGS_BEARER_TOKEN;
 const port = Number.parseInt(process.env.PORT ?? "4000");
 
 export const config = {
@@ -12,4 +13,5 @@ export const config = {
   warcraftLogsClientId,
   warcraftLogsClientSecret,
   port,
+  warcraftLogsBearerToken,
 };
