4de0dde62235f718641ac9fbb8e8fbc987be07a6
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-28 00:18:25 +0200

    improve table

--- apps/frontend/src/components/MythicPlusRunsTable.tsx
+++ apps/frontend/src/components/MythicPlusRunsTable.tsx
@@ -8,9 +8,11 @@ import {
   Table,
   Title,
   AspectRatio,
+  Anchor,
 } from "@mantine/core";
 import { MythicPlusRun } from "../graphql/graphql";
 import { TWW_S3_DUNGEONS } from "../data/dungeons_tww_s3";
+import { IconStarFilled } from "@tabler/icons-react";
 
 type MythicPlusRunsTableProps = {
   characterRuns: MythicPlusRun[];
@@ -21,15 +23,25 @@ export const MythicPlusRunsTable: React.FC<MythicPlusRunsTableProps> = ({
   characterRuns,
   isFetching = false,
 }) => {
-  console.log("characterRuns", characterRuns);
-
-  const rows = TWW_S3_DUNGEONS.map((dungeon) => {
+  // combine characterRuns with TWW_S3_DUNGEONS and sort by key level desc
+  const combinedRuns = TWW_S3_DUNGEONS.map((dungeon) => {
     const run = characterRuns.find(
       (run) => run.challange_mode_id === dungeon.challenge_mode_id,
     );
+    return {
+      ...dungeon,
+      key_level: run?.key_level ?? 0,
+      completed_at: run?.completed_at ?? null,
+      keystone_upgrades: run?.keystone_upgrades ?? 0,
+      runUrl: run?.url ?? null,
+    };
+  });
 
-    const completedAt = run?.completed_at
-      ? new Date(run.completed_at).toLocaleDateString()
+  combinedRuns.sort((a, b) => b.key_level - a.key_level);
+
+  const rows = combinedRuns.map((dungeon) => {
+    const completedAt = dungeon.completed_at
+      ? new Date(dungeon.completed_at).toLocaleDateString()
       : "-";
     return (
       <Table.Tr key={dungeon.id}>
@@ -38,33 +50,54 @@ export const MythicPlusRunsTable: React.FC<MythicPlusRunsTableProps> = ({
             <AspectRatio ratio={1} w={25}>
               <Image src={dungeon.icon_url} alt={dungeon.name} />
             </AspectRatio>
-            <Text>{dungeon.name}</Text>
+            {dungeon.runUrl ? (
+              <Anchor
+                size="sm"
+                m={0}
+                href={dungeon.runUrl ?? "#"}
+                target="_blank"
+                underline="never"
+              >
+                {dungeon.name}
+              </Anchor>
+            ) : (
+              <Text size="sm" m={0}>
+                {dungeon.name}
+              </Text>
+            )}
           </Group>
         </Table.Td>
         <Table.Td>
-          <Skeleton visible={isFetching}>
-            <Text size="sm">{run?.key_level}</Text>
+          <Skeleton
+            visible={isFetching}
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <Group gap={4}>
+              <Text size="sm" m={0}>
+                {dungeon.key_level ?? "-"}
+              </Text>
+
+              {Array.from({ length: dungeon?.keystone_upgrades ?? 0 }).map(
+                (_, i) => (
+                  <IconStarFilled key={i} size={10} color="gold" />
+                ),
+              )}
+            </Group>
           </Skeleton>
         </Table.Td>
         <Table.Td>
           <Skeleton visible={isFetching}>
-            <Text size="sm">{completedAt}</Text>
+            <Text size="sm" m={0}>
+              {completedAt}
+            </Text>
           </Skeleton>
         </Table.Td>
       </Table.Tr>
     );
   });
-
-  // Sort rows by key level descending
-  rows.sort((a, b) => {
-    const aKeyLevel = parseInt(a.props.children[1].props.children);
-    const bKeyLevel = parseInt(b.props.children[1].props.children);
-    return (bKeyLevel || 0) - (aKeyLevel || 0);
-  });
-
   return (
     <Stack w={"100%"} gap={0}>
-      <Title order={3}>Top Mythic+ Runs (Season 3)</Title>
+      <Title order={3}>Top Mythic+ Runs</Title>
       <Paper withBorder>
         <Table>
           <Table.Thead>
