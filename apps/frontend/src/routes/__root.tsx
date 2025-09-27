225ba2f04c53e8205c66298a729bbf0e2289f0ee
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-27 16:04:06 +0200

    programatically add analytics

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -1,10 +1,31 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanstackDevtools } from "@tanstack/react-devtools";
+import { useEffect } from "react";
+
+const Analytics: React.FC = () => {
+  useEffect(() => {
+    if (import.meta.env.PROD) {
+      const script = document.createElement("script");
+      script.src =
+        "https://puginspect-backend-8fde8e3dd345.herokuapp.com/stats.js";
+      script.defer = true;
+      script.dataset.websiteId = "92e835c5-3bbb-43a8-a358-5049b60d3b8c";
+      document.head.appendChild(script);
+
+      return () => {
+        document.head.removeChild(script);
+      };
+    }
+  }, []);
+
+  return null;
+};
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Analytics />
       <Outlet />
       <TanstackDevtools
         config={{
