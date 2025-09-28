91f8809494cba36e099ad20dc8b2625ac295b39d
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-09-28 23:15:47 +0200

    feat: implement annoyed messages for repeated pastes and update navigation handling

--- apps/frontend/src/components/Page.tsx
+++ apps/frontend/src/components/Page.tsx
@@ -1,39 +1,24 @@
 import { AppShell, Typography } from "@mantine/core";
 import Header from "./Header";
 import { useEffect, useRef } from "react";
-import { useParams, useRouter } from "@tanstack/react-router";
+import { useNavigate, useParams } from "@tanstack/react-router";
 import { parseRaiderIoUrl } from "../util/util";
 import { notifications } from "@mantine/notifications";
+import { annoyedMessages } from "../data/annoyedMessagegs";
 
 export const Page: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const router = useRouter();
+  const navigate = useNavigate();
   const params = useParams({
     from: "/$region/$realm/$name",
     shouldThrow: false,
   });
 
   const pasteCounter = useRef(0);
+  const paramsRef = useRef(params);
 
-  const annoyedMessages = [
-    "You are already viewing this character. Doh!",
-    "Seriously, you're *already here*...",
-    "Stop pasting the same thing. It's getting old.",
-    "Do you think something will change? It's the same page!",
-    "Alright, now you're just messing with me...",
-    "I'm not going to tell you again. Stop it.",
-    "This is your last warning. Stop it now!",
-    "That's it! I'm calling the cops on you!",
-    "I'm done. I'm not going to respond anymore.",
-    "Why do you keep doing this to me?",
-    "Is this some kind of joke to you?",
-    "I'm starting to question our relationship.",
-    "Maybe try pasting something that isn't this character?",
-    "I'm not your personal character portal, you know.",
-    "If you paste this again, I'm going to start ignoring you.",
-    "This is getting old. Please find a new character to obsess over.",
-    "OK, last one. If you paste this again, I'm done.",
-    "FINE! Paste it again, I DARE YOU!",
-  ];
+  useEffect(() => {
+    paramsRef.current = params;
+  }, [params]);
 
   useEffect(() => {
     const handlePaste = (event: ClipboardEvent) => {
@@ -42,15 +27,17 @@ export const Page: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       if (pastedText) {
         const raiderIoUrl = parseRaiderIoUrl(pastedText);
         if (raiderIoUrl) {
-          const { region, realm, name } = raiderIoUrl;
           event.preventDefault();
+          const { region, realm, name } = raiderIoUrl;
+
           const normalizedRegion = region.toLowerCase();
           const normalizedRealm = realm.toLowerCase();
           const normalizedName = name.toLowerCase();
+
           if (
-            params?.region === normalizedRegion &&
-            params?.realm === normalizedRealm &&
-            params?.name === normalizedName
+            paramsRef.current?.region === normalizedRegion &&
+            paramsRef.current?.realm === normalizedRealm &&
+            paramsRef.current?.name === normalizedName
           ) {
             pasteCounter.current += 1;
             const messageIndex = Math.min(
@@ -58,13 +45,11 @@ export const Page: React.FC<{ children: React.ReactNode }> = ({ children }) => {
               annoyedMessages.length - 1,
             );
             if (pasteCounter.current > annoyedMessages.length) {
-              router
-                .navigate({
-                  href: "https://youtu.be/dQw4w9WgXcQ?si=ybyZ4Xqe06YwJuHG",
-                })
-                .finally(() => {
-                  pasteCounter.current = 0;
-                });
+              navigate({
+                href: "https://youtu.be/dQw4w9WgXcQ?si=ybyZ4Xqe06YwJuHG",
+              }).finally(() => {
+                pasteCounter.current = 0;
+              });
 
               return;
             }
@@ -72,6 +57,8 @@ export const Page: React.FC<{ children: React.ReactNode }> = ({ children }) => {
               title: "Already on this character",
               message: annoyedMessages[messageIndex],
               color: "yellow",
+
+              autoClose: 2500,
             });
 
             return;
@@ -79,7 +66,7 @@ export const Page: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
           pasteCounter.current = 0;
 
-          router.navigate({
+          navigate({
             to: `/${normalizedRegion}/${normalizedRealm}/${normalizedName}`,
           });
         }
