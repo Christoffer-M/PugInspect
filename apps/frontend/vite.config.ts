c61fb3d1a026af8d4cfa5fa1d170306a09078466
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-10-19 11:48:20 +0200

    update packages and setup devtools properly

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tanstackRouter from "@tanstack/router-plugin/vite";
+import { devtools } from "@tanstack/devtools-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    devtools(),
+    tanstackRouter({ autoCodeSplitting: true }),
+    viteReact(),
+  ],
   server: {
     watch: {
       usePolling: true,
