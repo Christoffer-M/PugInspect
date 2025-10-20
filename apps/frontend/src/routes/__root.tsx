3242ff4d2d3d78b0cf37801309296d3f420c5027
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-10-20 19:19:47 +0200

    fix: update script source in Analytics component to use config.apiUrl

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -2,13 +2,13 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useEffect } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { config } from "../config ";
 
 const Analytics: React.FC = () => {
   useEffect(() => {
     if (import.meta.env.PROD) {
       const script = document.createElement("script");
-      script.src =
-        "https://puginspect-backend-8fde8e3dd345.herokuapp.com/stats.js";
+      script.src = config.apiUrl + "/stats.js";
       script.defer = true;
       script.dataset.websiteId = "92e835c5-3bbb-43a8-a358-5049b60d3b8c";
       document.head.appendChild(script);
