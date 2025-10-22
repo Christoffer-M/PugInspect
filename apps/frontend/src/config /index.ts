0875ec3bbe479a69a7cef459c0d17e0ffa62fe7b
Christoffer Mouritzen <christoffer_rm@outlook.com>
2025-10-22 15:58:19 +0200

    Set default URLs for GraphQL and API

--- apps/frontend/src/config /index.ts
+++ apps/frontend/src/config /index.ts
@@ -1,4 +1,4 @@
 export const config = {
-  graphqlUrl: import.meta.env.VITE_GRAPHQL_URL,
-  apiUrl: import.meta.env.VITE_API_URL,
+  graphqlUrl: import.meta.env.VITE_GRAPHQL_URL ?? "http://localhost:4000/graphql",
+  apiUrl: import.meta.env.VITE_API_URL ?? "http://localhost:4000",
 };
