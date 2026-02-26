41bcef4156cf7c5be6222c43d89e271578489c85
Christoffer Ry Mouritzen <christoffer_rm@outlook.com>
2026-02-27 00:09:20 +0100

    refactor: adjust table header properties and align text in Home component

--- apps/frontend/src/components/MythicPlusTables/components/RunTableHeader.tsx
+++ apps/frontend/src/components/MythicPlusTables/components/RunTableHeader.tsx
@@ -7,7 +7,7 @@ const RunTableHeader: React.FC = () => (
   <Table.Thead>
     <Table.Tr>
       <Table.Th w={DungeonNameMaxWidth}>Dungeon Name</Table.Th>
-      <Table.Th>Level</Table.Th>
+      <Table.Th miw={90}>Level</Table.Th>
       <Table.Th>Spec</Table.Th>
       <Table.Th>Date</Table.Th>
     </Table.Tr>
--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -8,10 +8,10 @@ const Home: React.FC = () => {
     <Page>
       <Container>
         <Center h={"80vh"}>
-          <Flex direction="column" align="center">
+          <Flex direction="column" align="center" ta={'center'}>
             <Flex direction="column" align="center">
-              <Title order={1} ta={'center'}>Welcome to PugInspect</Title>
-              <Title order={4} style={{ textAlign: "center" }} mt="sm">
+              <Title order={1} >Welcome to PugInspect</Title>
+              <Title order={4} mt="sm">
                 Quickly view WoW character stats, RIO scores, and raid logs
               </Title>
             </Flex>
@@ -21,7 +21,8 @@ const Home: React.FC = () => {
               <Text
                 mt="md"
                 c="dimmed"
-                style={{ maxWidth: "100%", textAlign: "center" }}
+
+                style={{ maxWidth: "100%" }}
               >
                 Start by typing in a character name above to search for a
                 character. You can also paste a Raider.IO character profile URL.
