9031a394123b410b87b02b4ef8a7b008cecde392
Christoffer Ry Mouritzen <christoffer_rm@outlook.com>
2026-03-04 18:23:12 +0100

    update config

--- apps/frontend/src/config /index.ts
+++ apps/frontend/src/config /index.ts
@@ -1,4 +1,4 @@
 export const config = {
-  graphqlUrl: import.meta.env.VITE_GRAPHQL_URL ?? "http://localhost:4000/graphql",
-  apiUrl: import.meta.env.VITE_API_URL ?? "http://localhost:4000",
+  graphqlUrl: import.meta.env.VITE_GRAPHQL_URL ?? "/graphql",
+  apiUrl: import.meta.env.VITE_API_URL ?? "/",
 };
