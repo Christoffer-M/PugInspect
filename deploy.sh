367c422364058cbc49bf0a05ca1a20bf92eb148f
Christoffer Ry Mouritzen <christoffer_rm@outlook.com>
2026-03-04 19:15:53 +0100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,9 @@ echo "==> Building backend..."
 pnpm run build:backend -- --mode production
 
 echo "==> Building frontend..."
+sudo chown -R "$USER":"$USER" apps/frontend/dist 2>/dev/null || true
 pnpm run build:frontend
-
-# Set permissions for Nginx
-echo "Setting permissions for frontend..."
-sudo chown -R www-data:www-data apps/frontend/dist
-sudo chmod -R 755 apps/frontend/dist
+chmod -R 755 apps/frontend/dist
 
 echo "==> Restarting PM2..."
 pm2 restart ecosystem.config.cjs --update-env
