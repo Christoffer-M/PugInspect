5c4c329d80fe3c157a3ec53a8db102065b9286c1
Christoffer Ry Mouritzen <christoffer_rm@outlook.com>
2026-03-26 16:21:15 +0100

    fix umami bug

--- apps/frontend/src/config /index.ts
+++ apps/frontend/src/config /index.ts
@@ -1,4 +1,4 @@
 export const config = {
   graphqlUrl: import.meta.env.VITE_GRAPHQL_URL ?? "/graphql",
-  apiUrl: import.meta.env.VITE_API_URL ?? "/",
+  apiUrl: import.meta.env.VITE_API_URL ?? "",
 };
