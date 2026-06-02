ca880f4e21b14fd5181cd56f958cebca5337974a
Christoffer Ry Mouritzen <christoffer_rm@outlook.com>
2026-06-02 16:20:34 +0200

    add host

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -11,6 +11,7 @@ const Analytics: React.FC = () => {
       script.src = config.apiUrl + "/stats.js";
       script.defer = true;
       script.dataset.websiteId = config.umamiWebsiteId;
+      script.dataset.hostUrl = config.apiUrl || window.location.origin;
       document.head.appendChild(script);
 
       return () => {
